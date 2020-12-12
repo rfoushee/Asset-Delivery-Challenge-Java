69f83d2a5bc160a95f3725d4d9193ab09d16393e
Ryan Foushee <ryan.foushee@nike.com>
2020-12-12 11:12:30 -0800

    add comments

--- TicTacToe/src/main/java/rfoushee/engines/GameEngine.java
+++ TicTacToe/src/main/java/rfoushee/engines/GameEngine.java
@@ -7,10 +7,22 @@ import rfoushee.models.GameBoardV2;
 import rfoushee.models.WinningPlayer;
 import rfoushee.models.WinningPositions;
 
+/**
+ * The engine to run a game of TicTacToe
+ */
 public class GameEngine {
 
+    // 9 is the maximum number of moves
     private final static int MAX_NUMBER_OF_PLAYER_MOVES = 9;
 
+    /**
+     * Determines if the game is complete. A completed game has a winner or there
+     * are no more moves to be made.
+     *
+     * @param gameBoard The gameboard.
+     * @return boolean
+     * @throws IllegalArgumentException
+     */
     public static boolean isGameComplete(GameBoardV2 gameBoard) throws IllegalArgumentException {
         if (gameBoard == null) {
             throw new IllegalArgumentException();
@@ -20,6 +32,7 @@ public class GameEngine {
         for (HashSet<Integer> playersMove : gameBoard.getGameState()) {
             totalNumberOfMoves += playersMove.size();
         }
+
         if (totalNumberOfMoves == MAX_NUMBER_OF_PLAYER_MOVES) {
             return true;
         }
@@ -28,6 +41,14 @@ public class GameEngine {
         return winner != WinningPlayer.NONE;
     }
 
+    /**
+     * determines if there is winner of the game. If no winner returns
+     * WinningPlayer.NONE
+     *
+     * @param gameBoard
+     * @return WinningPlayer The winning player of WinningPlayer.NONE
+     * @throws IllegalArgumentException
+     */
     public static WinningPlayer getWinner(GameBoardV2 gameBoard) throws IllegalArgumentException {
         if (gameBoard == null) {
             throw new IllegalArgumentException();
